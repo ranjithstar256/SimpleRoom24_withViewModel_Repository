d52d24270f04ac4e713c51f5a8a4ed97fdfc51d3
Ranjith <ranjithstar256@gmail.com>
2024-12-04 21:22:13 +0530

    fixed bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-
+    kotlin("kapt")
 }
 
 android {
@@ -62,5 +62,5 @@ dependencies {
     implementation("androidx.room:room-common:2.6.1")
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
-    //kapt("androidx.room:room-compiler:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
 }
--- app/src/main/java/tm/ranjith/simpleroom24/MainActivity.kt
+++ app/src/main/java/tm/ranjith/simpleroom24/MainActivity.kt
@@ -3,12 +3,24 @@ package tm.ranjith.simpleroom24
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.room.Room
 import kotlinx.coroutines.launch
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +33,7 @@ class MainActivity : ComponentActivity() {
                 "templedbexpl.db"
             ).build()
 
-            ///saveTempleData(db)
+            saveTempleData(db)
         }
     }
 }
@@ -29,22 +41,37 @@ class MainActivity : ComponentActivity() {
 fun saveTempleData(db: TempleDatabase){
 
     val scope = rememberCoroutineScope()
+    var TempleName by remember { mutableStateOf("") }
+    var TempleLocation by remember { mutableStateOf("") }
+    var TempleMain by remember { mutableStateOf("") }
 
-    val templeOneData = Temple(
-        0,
-        "KasiVish",
-        "Kasi",
-        "Vish"
-    )
 
-    Button(onClick = {
 
-        scope.launch {
-            db.templedao().insertNewTemple(templeOneData)
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()) {
+        TextField(value = TempleName, onValueChange = { TempleName = it })
+        TextField(value = TempleLocation, onValueChange = { TempleLocation = it })
+        TextField(value = TempleMain, onValueChange = { TempleMain = it })
+        val templeOneData = Temple(
+            0,
+            TempleName,
+            TempleLocation,
+            TempleMain
+        )
+        Button(onClick = {
+
+            scope.launch {
+                db.templedao().insertNewTemple(templeOneData)
+            }
+
+        }) {
+            Text(text = "save")
         }
 
-    }) {
-        Text(text = "save")
     }
+
+
     
 }
